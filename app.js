ddb6c0c9cc1bf41b43748a3c93b72d5c5487cc55
Luisito <hernandezmercadoluisenrique@gmail.com>
2025-05-30 18:56:03 -0600

    [FIX] Se modifico app.js y se a;adio la funcion obtenerDatosAngular que scrapea la pagina

--- app.js
+++ app.js
@@ -71,4 +71,49 @@ async function navegarentrepaginas(){
 }
 
 
-navegarentrepaginas();
+//navegarentrepaginas();
+
+async function obtenerDatosAngular() {
+    //INSTANCIAR NAVEGADOR
+    const navegador = await puppeteer.launch({
+        headless: false,
+        slowMo: 500
+    });
+
+    //CREAR NUEVA PESTANA EN NAVEGADOR
+    const pagina = await navegador.newPage();
+
+    //VISITAR O ABRIR LA PAGINA
+    await pagina.goto('https://example.com/');
+
+    //OBTENER LOS DATOS
+    const datos = await pagina.evaluate(()=>{
+
+        const arregloResultados = [];
+
+        const titulo = document.querySelector('div>h1')?.innerText;
+        const parrafo = document.querySelector('div>p')?.innerText;
+        const enlace = document.querySelector('div>p:nth-child(3)')?.innerText;
+        
+
+    
+
+        const objetoResultado = {
+            titulo: titulo,
+            parrafo: parrafo,
+            enlace: {
+                enlace : enlace,
+
+            }
+        }
+
+        arregloResultados.push(objetoResultado);
+        return arregloResultados;
+    })
+    
+    console.log(':::datos:::', datos);
+
+    navegador.close()
+}
+
+obtenerDatosAngular()
